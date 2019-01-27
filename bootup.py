75084737c3dad957f378d56e628f1297cb19d1e4
RAVI PRAKASH <ravi.0531.rp@gmail.com>
2019-01-27 19:15:16 +0530

    Update bootup.py

--- bootup.py
+++ bootup.py
@@ -1,1 +1,11 @@
 # boot  up
+  # sudo crontab -e
+  
+  # @reboot python3 location of .py file
+  
+  # put a bit of delay in your program start
+  
+  
+  #sudo raspi config
+  
+  # boot options => console login
